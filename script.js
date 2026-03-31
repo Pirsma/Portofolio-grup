cc900ef5b6c91dbf0bc1f03a42b1550f4b61da03
Pirsma <simcoc118@gmail.com>
2026-04-01 04:05:27 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,20 @@
+// ANIMASI SKILL BAR
 const bars = document.querySelectorAll(".bar span");
 
 window.addEventListener("scroll", () => {
-
-bars.forEach(bar => {
-
-const rect = bar.getBoundingClientRect();
-
-if(rect.top < window.innerHeight){
-
-bar.style.width = bar.dataset.width;
-
-}
-
+  bars.forEach(bar => {
+    const rect = bar.getBoundingClientRect();
+    if(rect.top < window.innerHeight){
+      bar.style.width = bar.dataset.width;
+    }
+  });
 });
 
+// FORM (GANTI PHP)
+document.getElementById("contactForm").addEventListener("submit", function(e){
+  e.preventDefault();
+
+  alert("Pesan berhasil dikirim! (Simulasi)");
+
+  this.reset();
 });
