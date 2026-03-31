042c1d81eb734f80526c4829f843d0d0835b447a
Pirsma <simcoc118@gmail.com>
2026-04-01 04:17:19 +0900

    Fix skill bar animation and update form submission
    Refactor skill bar animation to trigger on load and scroll events.

--- script.js
+++ script.js
@@ -1,20 +1,26 @@
-// ANIMASI SKILL BAR
+// ANIMASI SKILL BAR (FIX DESKTOP)
 const bars = document.querySelectorAll(".bar span");
 
-window.addEventListener("scroll", () => {
+function showBars() {
   bars.forEach(bar => {
     const rect = bar.getBoundingClientRect();
-    if(rect.top < window.innerHeight){
+
+    if(rect.top < window.innerHeight - 50){
       bar.style.width = bar.dataset.width;
     }
   });
-});
+}
 
-// FORM (GANTI PHP)
+// jalan saat load
+window.addEventListener("load", showBars);
+
+// jalan saat scroll
+window.addEventListener("scroll", showBars);
+
+
+// FORM
 document.getElementById("contactForm").addEventListener("submit", function(e){
   e.preventDefault();
-
   alert("Pesan berhasil dikirim! (Simulasi)");
-
   this.reset();
 });
